23d3a450868b9aee0a5e79dc2f36958e01b79324
Max Goedjen <max.goedjen@gmail.com>
2019-05-26 19:02:20 -0700

    Adding record storage

--- Sources/SlackBotKit/Camillink/Camillink+LinkMention.swift
+++ Sources/SlackBotKit/Camillink/Camillink+LinkMention.swift
@@ -10,16 +10,30 @@ extension CamillinkService {
         guard let linkURL = URL(string: linkString) else { return }
         guard try !isLinkWhitelisted(link: linkURL) else { return }
         if let previous = try previousLinkDiscussion(linkURL: linkURL) {
-            try sendPrompt(bot: bot, message: message, linkURL: linkURL, previousLink: previous)
+            try sendPrompt(bot: bot, message: message, linkURL: linkURL, previousLink: previous.permalink)
         } else {
-            try markPreviousDiscussion(message: message, linkURL: linkURL)
+            try markPreviousDiscussion(bot: bot, message: message, linkURL: linkURL)
         }
 
     }
     
-    func previousLinkDiscussion(linkURL: URL) throws -> URL? {
-        let urlString: String = try storage.get(key: linkURL.absoluteString, from: Keys.namespace, or: "INVALID")
-        return URL(string: urlString)
+    func previousLinkDiscussion(linkURL: URL) throws -> Record? {
+        let string = try storage.get(key: linkURL.absoluteString, from: Keys.namespace, or: "INVALID")
+        guard let data = string.data(using: .utf8) else { return nil }
+        let record = try JSONDecoder().decode(Record.self, from: data)
+        if let dayLimit = config.recencyLimitInDays {
+            // Dave please don't yell at me
+            guard let daysSince = Calendar.current.dateComponents([.day], from: record.date, to: Date()).day else { return nil }
+            if dayLimit > daysSince {
+                return record
+            } else {
+                // Past limit, remove it
+                storage.remove(key: linkURL.absoluteString, from: Keys.namespace)
+                return nil
+            }
+        } else {
+            return record
+        }
     }
     
     func sendPrompt(bot: SlackBot, message: MessageDecorator, linkURL: URL, previousLink: URL) throws {
@@ -31,10 +45,15 @@ extension CamillinkService {
         try bot.send(response.makeChatMessage())
     }
     
-    func markPreviousDiscussion(message: MessageDecorator, linkURL: URL    ) throws {
+    func markPreviousDiscussion(bot: SlackBot, message: MessageDecorator, linkURL: URL) throws {
         guard let channel = message.message.channel else { return }
-        // TODO: MAXG pending Chameleon change to get permalink
-//        storage.set(value: channel.id, forKey: linkURL.absoluteString, in: Keys.namespace)
+        let permalink = try bot.permalink(message.message)
+        guard let permalinkURL = URL(string: permalink) else { return }
+        let record = Record(channelID: channel.id, permalink: permalinkURL)
+        // Hack since Storage only takes strings for the moment
+        let data = try JSONEncoder().encode(record)
+        guard let string = String(data: data, encoding: .utf8) else { return }
+        storage.set(value: string, forKey: linkURL.absoluteString, in: Keys.namespace)
     }
     
     func isLinkWhitelisted(link: URL) throws -> Bool {
---
+++ Sources/SlackBotKit/Camillink/Camillink+Storage.swift
@@ -0,0 +1,18 @@
+import Foundation
+
+extension CamillinkService {
+    
+    public struct Record: Codable {
+        
+        let date: Date
+        let channelID: String
+        let permalink: URL
+        
+        init(channelID: String, permalink: URL){
+            self.date = Date()
+            self.channelID = channelID
+            self.permalink = permalink
+        }
+        
+    }
+}
