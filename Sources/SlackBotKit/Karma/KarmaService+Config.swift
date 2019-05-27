9ed1d68f33ab4d3d17beb3807ec739ae9869a059
Zev Eisenberg <zeisenberg@rightpoint.com>
2019-05-27 19:04:06 -0400

    Pull out function.

--- Sources/SlackBotKit/Karma/KarmaService+Config.swift
+++ Sources/SlackBotKit/Karma/KarmaService+Config.swift
@@ -19,14 +19,27 @@ extension KarmaService {
                 positiveComments: [
                     { "You rock \($0)! Now at \($1)." },
                     { "Nice job, \($0)! Your karma just bumped to \($1)." },
-                    { "Awesome \($0)! You're now at \($1) point\($1 == 1 ? "" : "s")." } // lazy pluralization
+                    { "Awesome \($0)! You're now at \($1) \(pluralizedScoreString(from: $1))." }
                 ],
                 negativeComments: [
                     { "booooo \($0)! Now at \($1)." },
                     { "Tssss \($0). Dropped your karma to \($1)." },
-                    { "Sorry, but I have to drop \($0)'s karma down to \($1) point\($1 == 1 ? "" : "s")." }, // lazy pluralization
+                    { "Sorry, but I have to drop \($0)'s karma down to \($1) \(pluralizedScoreString(from: $1))." },
                 ]
             )
         }
     }
+}
+
+private extension KarmaService.Config {
+
+    /// Non-localized pluralization.
+    ///
+    /// - Parameter score: a point score.
+    /// - Returns: `"point"` if score is 1; otherwise, `"points"`.
+    /// - Note: Should be replaced by proper localized pluralization
+    static func pluralizedScoreString(from score: Int) -> String {
+        return score == 1 ? "point" : "points"
+    }
+
 }
