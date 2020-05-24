7e52c175bf81b941ab8fc5f25e130dc74d5f47d4
Ian Keen <iankeen82@gmail.com>
2020-05-24 10:01:15 -0700

    Update Chameleon

--- Package.swift
+++ Package.swift
@@ -1,20 +1,26 @@
-// swift-tools-version:4.0
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
+// swift-tools-version:5.2
 import PackageDescription
 
 let package = Package(
     name: "SlackBot",
+    platforms: [
+        .macOS(.v10_15)
+    ],
     products: [
         .executable(name: "SlackBot", targets: ["SlackBot"]),
         .library(name: "SlackBotKit", targets: ["SlackBotKit"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/ChameleonBot/Chameleon.git", .upToNextMinor(from: "0.0.3")),
+        .package(url: "https://github.com/ChameleonBot/Chameleon.git", .branch("revamp")),
+        .package(url: "https://github.com/mxcl/LegibleError.git", from: "1.0.0"),
     ],
     targets: [
-        .target(name: "SlackBot", dependencies: ["SlackBotKit"]),
-        .target(name: "SlackBotKit", dependencies: ["Chameleon"]),
+        .target(name: "SlackBot", dependencies: [
+            "SlackBotKit",
+            .product(name: "VaporProviders", package: "Chameleon"),
+            .product(name: "LegibleError", package: "LegibleError"),
+        ]),
+        .target(name: "SlackBotKit", dependencies: [.product(name: "ChameleonKit", package: "Chameleon")]),
         .testTarget(name: "SlackBotKitTests", dependencies: ["SlackBotKit"]),
     ]
 )
