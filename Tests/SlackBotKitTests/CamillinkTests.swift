013d3bf8685fc040fd68b6bc0c9481fbbf5f89b6
Ian Keen <iankeen82@gmail.com>
2020-06-21 14:08:48 -0700

    Update tests

--- Tests/SlackBotKitTests/CamillinkTests.swift
+++ Tests/SlackBotKitTests/CamillinkTests.swift
@@ -18,92 +18,47 @@ class CamillinkTests: XCTestCase {
         XCTAssertClear(test)
 
         // messages without links aren't triggering
-        try test.send(.event(
-            .message(userId: "1", channelId: "1", kind: .channel, [
-                .text("hello world")
-            ])
-        ))
+        try test.send(.event(.message([.text("hello world")])))
         try XCTAssertEqual(storage.keys(in: SlackBot.Camillink.Keys.namespace), [])
         XCTAssertClear(test)
 
         // initial link
-        try test.enqueue([.permalink(channelId: "1", url: URL("https://www.slack.com/permalink/channel1/link1"))])
-        try test.send(.event(
-            .message(userId: "1", channelId: "1", kind: .channel, [
-                .link(url: URL("https://www.link1.com"))
-            ])
-        ))
-        try XCTAssertEqual(storage.keys(in: SlackBot.Camillink.Keys.namespace), ["https://www.link1.com"])
+        try test.enqueue([.permalink(channelId: "C0000000000", url: URL("https://www.slack.com/permalink/channel1/link1"))])
+        try test.send(.event(.messageWithLink1()))
+        try XCTAssertEqual(storage.keys(in: SlackBot.Camillink.Keys.namespace), ["https://twitter.com/IanKay"])
+        XCTAssertClear(test)
+
+        try test.send(.event(.unfurlLink1()))
         XCTAssertClear(test)
 
         // same link, same channel, 1 second later
         date.addTimeInterval(1)
         try test.enqueue([.emptyMessage()])
-        try test.send(.event(
-            .message(userId: "1", channelId: "1", kind: .channel, [
-                .link(url: URL("https://www.link1.com"))
-            ])
-        ))
-        try XCTAssertEqual(storage.keys(in: SlackBot.Camillink.Keys.namespace), ["https://www.link1.com"])
+        try test.send(.event(.messageWithLink1()))
+        try XCTAssertEqual(storage.keys(in: SlackBot.Camillink.Keys.namespace), ["https://twitter.com/IanKay"])
+        XCTAssertClear(test)
+
+        // same link, thread
+        try test.enqueue([.emptyMessage()])
+        try test.send(.event(.threadedMessageWithLink1()))
+        XCTAssertClear(test)
+
+        // delete link from thread
+        try test.send(.event(.deleteThreadedMessageWithLink1()))
         XCTAssertClear(test)
 
         // same link, different channel, 1 second later
         date.addTimeInterval(1)
         try test.enqueue([.emptyMessage()])
-        try test.send(.event(
-            .message(userId: "1", channelId: "2", kind: .channel, [
-                .link(url: URL("https://www.link1.com"))
-            ])
-        ))
-        try XCTAssertEqual(storage.keys(in: SlackBot.Camillink.Keys.namespace), ["https://www.link1.com"])
+        try test.send(.event(.message([.link(url: URL("https://twitter.com/IanKay"))])))
+        try XCTAssertEqual(storage.keys(in: SlackBot.Camillink.Keys.namespace), ["https://twitter.com/IanKay"])
         XCTAssertClear(test)
 
         // same link, different channel, after expiration
         let day = 60 * 60 * 24
         date.addTimeInterval(TimeInterval(day * (config.recencyLimitInDays! + 1)))
-        try test.send(.event(
-            .message(userId: "1", channelId: "2", kind: .channel, [
-                .link(url: URL("https://www.link1.com"))
-            ])
-        ))
+        try test.send(.event(.message([.link(url: URL("https://twitter.com/IanKay"))])))
         try XCTAssertEqual(storage.keys(in: SlackBot.Camillink.Keys.namespace), [])
         XCTAssertClear(test)
-    }
-}
-
-extension FixtureSource {
-    public static func link(text: String? = nil, url: URL) throws -> FixtureSource<RichTextElement> {
-        return .init(raw: """
-        {
-            "type": "link",
-            "text": "\(text ?? "null")",
-            "url": "\(url.absoluteString)"
-        }
-        """)
-    }
-}
-
-extension RichTextFixture {
-    public static func link(text: String? = nil, url: URL) -> RichTextFixture {
-        let string: MarkdownString
-
-        switch text {
-        case let text?: string = "\("\(text)", url)"
-        case nil: string = "\(url)"
-        }
-
-        return .init { try (string, Message.Layout.RichText.Element.Link(from: .link(text: text, url: url))) }
-    }
-}
-
-extension FixtureSource {
-    public static func permalink(channelId: String, url: URL) -> FixtureSource<SlackDispatcher> {
-        return .init(raw: """
-        {
-            "ok": true,
-            "channel": "\(channelId)",
-            "permalink": "\(url.absoluteString)"
-        }
-        """)
     }
 }
